54dfe6ca8f0c5f76436a44ffe628da2998fa8965
wangyu <wangyu@licaigc.com>
2021-01-22 18:17:14 +0800

    first commit

---
+++ macProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  macProject
+//
+//  Created by wy on 2021/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
